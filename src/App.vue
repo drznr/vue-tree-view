6ab2a98a044753afd4ba537832050de21085af8f
daniel.dresner <daniel.dresner@agmatix.com>
2024-02-05 11:47:41 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import treeView from './tree-view/tree-view.vue';
+import atc from './assets/atc.json';
+</script>
+
+<template>
+    <tree-view :nodes="atc" />
+</template>
