dde28c4e1391152a24b9d1bac2b59f999e4e29fe
daniel.dresner <daniel.dresner@agmatix.com>
2024-02-05 15:53:20 +0200

    debounce utils

--- src/tree-view/utils.spec.ts
+++ src/tree-view/utils.spec.ts
@@ -1,5 +1,5 @@
 import { MOCK_TREE } from './__mocks__/tree.mock';
-import { traverse } from './utils';
+import { debounce, traverse } from './utils';
 
 describe('Tree View Utils', () => {
   describe('traverse()', () => {
@@ -11,6 +11,34 @@ describe('Tree View Utils', () => {
       expect(handlerSpy).toHaveBeenCalledTimes(19);
       expect(handlerSpy).toHaveBeenNthCalledWith(1, expect.objectContaining({ id: '1' }));
       expect(handlerSpy).toHaveBeenLastCalledWith(expect.objectContaining({ id: '10011' }));
+    });
+  });
+
+  describe('debounce()', () => {
+    beforeEach(() => {
+      jest.useFakeTimers();
+    });
+
+    afterEach(() => {
+      jest.useRealTimers();
+    });
+
+    it('should debounce function calls', () => {
+      const callback = jest.fn();
+      const debounced = debounce(callback, 500);
+
+      debounced();
+      expect(callback).not.toHaveBeenCalled();
+
+      jest.advanceTimersByTime(100);
+      debounced();
+      expect(callback).not.toHaveBeenCalled();
+
+      jest.advanceTimersByTime(499);
+      expect(callback).not.toHaveBeenCalled();
+
+      jest.advanceTimersByTime(1);
+      expect(callback).toHaveBeenCalledTimes(1);
     });
   });
 });
--- src/tree-view/utils.ts
+++ src/tree-view/utils.ts
@@ -7,3 +7,18 @@ export function traverse(node: INode, handler: Function, depth = 0) {
     node.children.forEach(childNode => traverse(childNode, handler, depth + 1));
   }
 }
+
+export function debounce(func: Function, ms: number) {
+  let timeout: ReturnType<typeof setTimeout> | undefined;
+
+  return function (...args: unknown[]) {
+    const context = this;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(function () {
+      timeout = undefined;
+      func.apply(context, args);
+    }, ms);
+  };
+}
