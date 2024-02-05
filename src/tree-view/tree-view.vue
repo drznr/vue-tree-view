90d182f9c49411a02093d8cd0a71c620707c1599
daniel.dresner <daniel.dresner@agmatix.com>
2024-02-05 12:48:52 +0200

    expand / collapse all handlers

--- src/tree-view/tree-view.vue
+++ src/tree-view/tree-view.vue
@@ -2,6 +2,7 @@
 import { computed, ref } from 'vue';
 import { INode } from './types';
 import treeNode from './components/tree-node.vue';
+import { traverse } from './utils';
 
 const props = defineProps<{
   nodes: INode | INode[];
@@ -22,8 +23,18 @@ function expandNode(node: INode) {
 
   expandedChildsMap.value.set(node.id, true);
 }
+
+function expandAll() {
+  const handler = (node: INode) => expandedChildsMap.value.set(node.id, true);
+  data.value.forEach(node => traverse(node, handler));
+}
+
+function collapseAll() {
+  expandedChildsMap.value.clear();
+}
 </script>
 
 <template>
+  <slot name="controls" :expandAll="expandAll" :collapseAll="collapseAll" />
   <tree-node v-for="node in data" :node="node" :expanded-map="expandedChildsMap" @expand="expandNode" />
 </template>
