c0b43695c3589e3e0741c4f8bd07478bb0938a3e
daniel.dresner <daniel.dresner@agmatix.com>
2024-02-06 14:40:33 +0200

    added vue tsc for ts error indication in console

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslint from 'vite-plugin-eslint';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslint({ lintOnStart: true, failOnError: false, failOnWarning: false })],
+  plugins: [vue(), eslint(), checker({ vueTsc: true })],
 });
