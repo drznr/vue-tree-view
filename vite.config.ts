67f6aecbf6b9e1044ffc38adaa0e72596d0af7c7
daniel.dresner <daniel.dresner@agmatix.com>
2024-02-11 13:29:48 +0200

    coverage config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,15 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslint from 'vite-plugin-eslint';
 import checker from 'vite-plugin-checker';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
+    coverage: {
+      provider: 'v8',
+      exclude: [...(configDefaults.coverage.exclude as string[]), '**/__mocks__/**', '**/types.ts/**'],
+    },
   },
   plugins: [vue(), eslint(), checker({ vueTsc: true })],
 });
