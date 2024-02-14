5acb41308ac372de803d48d13b450d1f94889e44
daniel.dresner <daniel.dresner@agmatix.com>
2024-02-14 13:16:50 +0200

    use test id for finding node in testing

--- src/tree-view/tree-view.spec.ts
+++ src/tree-view/tree-view.spec.ts
@@ -1,56 +1,56 @@
-import { mount } from '@vue/test-utils';
+import { mount, type ComponentMountingOptions } from '@vue/test-utils';
 import treeView from './tree-view.vue';
 import { MOCK_TREE } from './__mocks__/tree.mock';
 import { type TestWrapper } from '../../vitest.setup';
+import { type INode } from './types';
+import { TREE_NODE_TEST_ID } from './components/tree-node.vue';
 
 describe('<tree-view />', () => {
+  const mountComponent = (options?: ComponentMountingOptions<typeof treeView>) => {
+    return mount(treeView, {
+      props: {
+        nodes: MOCK_TREE,
+      },
+      ...options,
+    }) as unknown as TestWrapper<typeof treeView>;
+  };
+
   describe('expanding', () => {
     it('should render root level by default', () => {
-      const wrapper = mount(treeView, {
-        props: {
-          nodes: MOCK_TREE,
-        },
-      });
+      const wrapper = mountComponent();
 
-      const nodes = wrapper.findAll('li');
+      const nodes = wrapper.findAllByTestId(TREE_NODE_TEST_ID);
 
       expect(nodes).toHaveLength(1);
     });
 
     it('should render all nodes if specified | defaultExpandAll prop', () => {
-      const wrapper = mount(treeView, {
+      const wrapper = mountComponent({
         props: {
           nodes: MOCK_TREE,
           defaultExpandAll: true,
         },
-      }) as unknown as TestWrapper;
+      });
 
-      const nodes = wrapper.findAll('li');
+      const nodes = wrapper.findAllByTestId(TREE_NODE_TEST_ID);
 
       expect(nodes).toHaveLength(19);
       expect(wrapper.findByText('Chinese softshell turtle')).toBeTruthy();
     });
 
     it('should allow expanding all nodes | expandAll', async () => {
-      const wrapper = mount(treeView, {
-        props: {
-          nodes: MOCK_TREE,
-        },
-      }) as unknown as TestWrapper<typeof treeView>;
+      const wrapper = mountComponent();
 
-      expect(wrapper.findAll('li')).toHaveLength(1);
+      expect(wrapper.findAllByTestId(TREE_NODE_TEST_ID)).toHaveLength(1);
 
       wrapper.vm.expandAll();
       await wrapper.vm.$nextTick();
 
-      expect(wrapper.findAll('li')).toHaveLength(19);
+      expect(wrapper.findAllByTestId(TREE_NODE_TEST_ID)).toHaveLength(19);
     });
 
     it('should allow manual expanding of nodes', async () => {
-      const wrapper = mount(treeView, {
-        props: {
-          nodes: MOCK_TREE,
-        },
+      const wrapper = mountComponent({
         slots: {
           controls: '',
           'node-content': `
@@ -59,19 +59,42 @@ describe('<tree-view />', () => {
             </template>
           `,
         },
-      }) as unknown as TestWrapper<typeof treeView>;
+      });
 
-      expect(wrapper.findAll('li')).toHaveLength(1);
+      expect(wrapper.findAllByTestId(TREE_NODE_TEST_ID)).toHaveLength(1);
 
       // open root node
       await wrapper.find('.slotted-el').trigger('click');
-      expect(wrapper.findAll('li')).toHaveLength(3);
+      expect(wrapper.findAllByTestId(TREE_NODE_TEST_ID)).toHaveLength(3);
 
       // close root node
       await wrapper.find('.slotted-el').trigger('click');
-      expect(wrapper.findAll('li')).toHaveLength(1);
+      expect(wrapper.findAllByTestId(TREE_NODE_TEST_ID)).toHaveLength(1);
+    });
+
+    it('should expand to matching nodes by given search fn', async () => {
+      const wrapper = mountComponent();
+
+      expect(wrapper.findAllByTestId(TREE_NODE_TEST_ID)).toHaveLength(1);
+
+      wrapper.vm.search((node: INode) => !!node.name?.includes('lizard'));
+      await wrapper.vm.$nextTick();
+
+      expect(wrapper.findAllByTestId(TREE_NODE_TEST_ID)).toHaveLength(8);
+    });
+
+    it('should expand to selected nodes', async () => {
+      const wrapper = mountComponent({
+        props: {
+          nodes: MOCK_TREE,
+          modelValue: ['10006', '10007'],
+        },
+      });
+
+      wrapper.vm.expandToSelection();
+      await wrapper.vm.$nextTick();
+
+      expect(wrapper.findAllByTestId(TREE_NODE_TEST_ID)).toHaveLength(8);
     });
   });
-
-  describe('', () => {});
 });
