9afebb9f792a79cc43ce8766e04d21a1139521c0
daniel.dresner <daniel.dresner@agmatix.com>
2024-02-14 12:16:33 +0200

    vitest setup & expandig functionality coverage

--- src/tree-view/tree-view.spec.ts
+++ src/tree-view/tree-view.spec.ts
@@ -1,6 +1,7 @@
 import { mount } from '@vue/test-utils';
 import treeView from './tree-view.vue';
 import { MOCK_TREE } from './__mocks__/tree.mock';
+import { type TestWrapper } from '../../vitest.setup';
 
 describe('<tree-view />', () => {
   describe('expanding', () => {
@@ -15,5 +16,62 @@ describe('<tree-view />', () => {
 
       expect(nodes).toHaveLength(1);
     });
+
+    it('should render all nodes if specified | defaultExpandAll prop', () => {
+      const wrapper = mount(treeView, {
+        props: {
+          nodes: MOCK_TREE,
+          defaultExpandAll: true,
+        },
+      }) as unknown as TestWrapper;
+
+      const nodes = wrapper.findAll('li');
+
+      expect(nodes).toHaveLength(19);
+      expect(wrapper.findByText('Chinese softshell turtle')).toBeTruthy();
+    });
+
+    it('should allow expanding all nodes | expandAll', async () => {
+      const wrapper = mount(treeView, {
+        props: {
+          nodes: MOCK_TREE,
+        },
+      }) as unknown as TestWrapper<typeof treeView>;
+
+      expect(wrapper.findAll('li')).toHaveLength(1);
+
+      wrapper.vm.expandAll();
+      await wrapper.vm.$nextTick();
+
+      expect(wrapper.findAll('li')).toHaveLength(19);
+    });
+
+    it('should allow manual expanding of nodes', async () => {
+      const wrapper = mount(treeView, {
+        props: {
+          nodes: MOCK_TREE,
+        },
+        slots: {
+          controls: '',
+          'node-content': `
+            <template #node-content="scope">
+              <div @click="scope.toggleExpand" class="slotted-el" />
+            </template>
+          `,
+        },
+      }) as unknown as TestWrapper<typeof treeView>;
+
+      expect(wrapper.findAll('li')).toHaveLength(1);
+
+      // open root node
+      await wrapper.find('.slotted-el').trigger('click');
+      expect(wrapper.findAll('li')).toHaveLength(3);
+
+      // close root node
+      await wrapper.find('.slotted-el').trigger('click');
+      expect(wrapper.findAll('li')).toHaveLength(1);
+    });
   });
+
+  describe('', () => {});
 });
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,23 @@
+import { config, type VueWrapper, createWrapperError, DOMWrapper } from '@vue/test-utils';
+import type { VueNode } from '@vue/test-utils/dist/types';
+
+export type TestWrapper<T = VueNode> = VueWrapper<T> & ReturnType<typeof customBehaviors>;
+
+const customBehaviors = (wrapper: VueWrapper) => ({
+  findByText(text: string, selector = '*'): DOMWrapper<Element> {
+    const elements = wrapper.findAll(selector);
+    return elements.find(el => el.text().trim() === text.trim()) ?? createWrapperError('DOMWrapper');
+  },
+  findAllByText(text: string, selector = '*') {
+    return wrapper.findAll(selector).filter(el => el.text().trim() === text.trim());
+  },
+  findByTestId(testId: string) {
+    return wrapper.get(`[data-testid="${testId}"]`);
+  },
+  findAllByTestId(testId: string) {
+    return wrapper.findAll(`[data-testid="${testId}"]`);
+  },
+});
+
+config.plugins.VueWrapper.install(customBehaviors);
 global.structuredClone = (value: unknown) => JSON.parse(JSON.stringify(value));
