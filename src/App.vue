df71df3200ac0e447983525d8b203f1a0077b519
daniel.dresner <daniel.dresner@agmatix.com>
2024-02-15 13:21:52 +0200

    deleted index.html main.js App.vue

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import treeView from './tree-view/tree-view.vue';
-import atc from './assets/atc.json';
-</script>
-
-<template>
-    <tree-view :nodes="atc" />
-</template>
