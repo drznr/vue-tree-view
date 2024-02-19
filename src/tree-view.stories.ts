e70ec312adb75b440db716bdb04679716a44b13c
daniel.dresner <daniel.dresner@agmatix.com>
2024-02-19 13:13:03 +0200

    minor

--- src/tree-view.stories.ts
+++ src/tree-view.stories.ts
@@ -77,7 +77,7 @@ const BaseTemplate: Story = {
 
             <template #node-content="{ node, expanded, selected, indeterminate, toggleExpand, toggleSelection, fetching, error }">
                 <div class="flex flex-row items-center my-2" @click="toggleExpand">
-                <svg v-if="fetching" class="me-6" width="16px" height="16px" viewBox="0 0 32 32">
+                <svg v-if="fetching" class="me-4" width="16px" height="16px" viewBox="0 0 32 32">
                     <rect x="0" y="0" width="100%" height="100%" fill="#FFFFFF" />
                     <g>
                         <path d="M64 128A64 64 0 0 1 18.34 19.16L21.16 22a60 60 0 1 0 52.8-17.17l.62-3.95A64 64 0 0 1 64 128z" fill="#000000"/>
@@ -87,14 +87,14 @@ const BaseTemplate: Story = {
                 <span
                     v-else
                     :class="{ 'rotate-90': expanded, invisible: !node.children?.length }"
-                    class="me-6"
+                    class="me-4"
                 >
                 <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" fill="currentColor">
                     <path d="M3.414 1A2 2 0 0 0 0 2.414v11.172A2 2 0 0 0 3.414 15L9 9.414a2 2 0 0 0 0-2.828L3.414 1Z" />
                 </svg>
                 </span>
 
-                <span class="me-6" @click.stop="">
+                <span class="me-4" @click.stop="">
                     <input
                         type="checkbox"
                         :checked="selected"
@@ -107,9 +107,9 @@ const BaseTemplate: Story = {
                     {{ node.id }}
                 </span>
 
-                <span v-if="node.name" class="ms-6">{{ node.name }}</span>
+                <span v-if="node.name" class="ms-4">{{ node.name }}</span>
 
-                <span v-if="error" class="ms-6">someting went wrong!</span>
+                <span v-if="error" class="ms-4">someting went wrong!</span>
                 </div>
             </template>
         </tree-view>
