ad2871b2bb70235e8df9c444123a27a5c5f3449b
drznr <dresnerdaniel@gmail.com>
2024-02-29 16:12:16 +0200

    added jsdoc to props, events and exposed hadnlers

--- src/tree-view.vue
+++ src/tree-view.vue
@@ -6,41 +6,150 @@ import { debounce, getNodeId } from './utils';
 import { useTreeView } from './composables/use-tree-view';
 
 defineSlots<{
+  /**
+   * Slot above tree data, can be used to renders controls for handling tree operations
+   * has all global tree handlers in scope
+   */
   controls(props: {
+    /**
+     * Expand all tree nodes
+     */
     expandAll: VoidFunction;
+    /**
+     * Collapse all tree nodes
+     */
     collapseAll: VoidFunction;
+    /**
+     * Select all tree nodes
+     */
     selectAll: VoidFunction;
+    /**
+     * Unselect all tree nodes
+     */
     unselectAll: VoidFunction;
+    /**
+     * Expand tree nodes to see all selected nodes
+     */
     expandToSelection: VoidFunction;
+    /**
+     * Restore to initial nodes after filtering
+     */
     resetFilter: VoidFunction;
+    /**
+     * Filter tree nodes by query
+     * @param query - name of field and term or a boolean returning function to filter by
+     */
     filter: (query: TQueryBy<TNode>) => void;
+    /**
+     * Expand tree nodes by query
+     * @param query - name of field and term or a boolean returning function to search by
+     */
     search: (query: TQueryBy<TNode>) => void;
   }): unknown;
 
+  /**
+   * Slot for node itself, use for rendering node
+   * has scoped node and metadata about its state and node specific handlers
+   */
   ['node-content'](props: {
+    /**
+     * Node object
+     */
     node: TNode;
+    /**
+     * A flag for telling if node is expanded
+     */
     expanded: boolean;
+    /**
+     * A flag for telling if node is selected
+     * (or all childs are selected)
+     */
     selected: boolean;
+    /**
+     * A flag for telling if any child of a node is selected
+     */
     indeterminate: boolean;
+    /**
+     * A flag for telling fetchChildren prop is fetching
+     */
     fetching: boolean;
+    /**
+     * A flag for telling fetchChildren prop has failed
+     */
     error: boolean;
+    /**
+     * Toggle node expanding
+     */
     toggleExpand: VoidFunction;
-    toggleSelect: (isUnselect: boolean) => void;
-    asyncToggleSelect: (isUnselect: boolean) => Promise<void>;
+    /**
+     * Toggle node selection
+     * (will run recursively for all childs of none leaf nodes)
+     * @param isSelected - current state of node selection
+     */
+    toggleSelect: (isSelected: boolean) => void;
+    /**
+     * Async toggle node selection, will fetch node children
+     * (will run recursively for all childs of none leaf nodes)
+     * @param isSelected - current state of node selection
+     */
+    asyncToggleSelect: (isSelected: boolean) => Promise<void>;
   }): unknown;
 }>();
 
 const props = withDefaults(
   defineProps<{
+    /**
+     * Tree data structure object or array
+     * @required
+     */
     nodes: TNode | TNode[];
+    /**
+     * An array of selected nodes identifiers
+     * @default []
+     */
     modelValue?: string[];
+    /**
+     * Render all tree nodes by default
+     * @default false
+     */
     defaultExpandAll?: boolean;
+    /**
+     * Property name to be used as node identifier
+     * @default 'id'
+     */
     idKey?: keyof TNode;
+    /**
+     * Property name to be used as node children
+     * @default 'children'
+     */
     childrenKey?: keyof TNode;
+    /**
+     * number of milliseconds to be used for debouncing
+     * search and filter handlers, 0 will cancel debouncing
+     * @default 300
+     */
     debounceMs?: number;
+    /**
+     * number of milliseconds to be used for transitioning
+     * node expanding
+     * @default 300
+     */
     transitionMs?: number;
+    /**
+     * cancel transition, will improve performance, recommendad for big trees
+     * @default false
+     */
     noTransition?: boolean;
+    /**
+     * number of pixels to be used to indent tree nested levels
+     * @default 24
+     */
     indentPx?: number;
+    /**
+     * if supplied will be used to fetch node children on creating
+     * and to recursively select nodes
+     * @default undefined
+     */
     fetchChildren?: (nodeId: string) => Promise<TNode[] | null | undefined>;
   }>(),
   {
@@ -51,7 +160,18 @@ const props = withDefaults(
   }
 );
 
-const emit = defineEmits<{ 'update:modelValue': [payload: string[]]; 'on-error': [error: Error] }>();
+const emit = defineEmits<{
+  /**
+   * Change off selected nodes
+   * payload is a list of selected nodes
+   */
+  'update:modelValue': [payload: string[]];
+  /**
+   * Error of fetchChildren function of a specific node
+   * payload is an error with node details and original error reference
+   */
+  'on-error': [error: Error];
+}>();
 
 const childrenKey = computed(() => (props.childrenKey ?? 'children') as keyof TNode);
 const idKey = computed(() => (props.idKey ?? 'id') as keyof TNode);
@@ -76,15 +196,58 @@ const {
 } = useTreeView(props.nodes, props.modelValue, idKey.value, childrenKey.value, props.fetchChildren);
 
 defineExpose({
+  /**
+   * Expand all tree nodes
+   */
   expandAll,
+  /**
+   * Collapse all tree nodes
+   */
   collapseAll,
-  search,
-  toggleExpand,
-  expandToSelection,
-  filter,
-  resetFilter,
+  /**
+   * Select all tree nodes
+   */
   selectAll,
+  /**
+   * Unselect all tree nodes
+   */
   unselectAll,
+  /**
+   * Restore to initial nodes after filtering
+   */
+  expandToSelection,
+  /**
+   * Expand tree nodes by query
+   * @param query - name of field and term or a boolean returning function to search by
+   */
+  search,
+  /**
+   * Filter tree nodes by query
+   * @param query - name of field and term or a boolean returning function to filter by
+   */
+  filter,
+  /**
+   * Restore to initial nodes after filtering
+   */
+  resetFilter,
+  /**
+   * Toggle expanding of a specific node
+   * @param node - the node
+   */
+  toggleExpand,
+  /**
+   * Toggle selection of a specific node
+   * (will run recursively for all childs of none leaf nodes)
+   * @param node - the node
+   * @param isSelected - current state of node selection
+   */
+  toggleSelect,
+  /**
+   * Toggle selection of a specific node, will fetch node children
+   * (will run recursively for all childs of none leaf nodes)
+   * @param node - the node
+   * @param isSelected - current state of node selection
+   */
   asyncToggleSelect,
 });
 
